678193a8aa6d042ee255d5a68e01221309ea9ab1
JoaoTorrinhas <jptorrinhas@gmail.com>
2023-05-07 21:49:11 +0100

    update

--- src/serverSide/sharedRegions/GeneralRepos.java
+++ src/serverSide/sharedRegions/GeneralRepos.java
@@ -10,19 +10,6 @@ import genclass.TextFile;
 import java.util.Objects;
 
 
-/**
- * General Repository
- *
- * It is responsible to keep the visible internal state of the problem and to
- * provide means for it
- * to be printed in the logging file.
- * It is implemented as an implicit monitor.
- * All public methods are executed in mutual exclusion.
- * There are no internal synchronisation points.
- * Implementation of a client-server model of type 2 (server replication).
- * Communication is based on a communication channel under the TCP protocol.
- */
-
 public class GeneralRepos {
     
     /**
